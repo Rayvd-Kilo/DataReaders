6852c8a9820d80f11dc91597d1ba4c8f8a0fbde7
Rayvd Kilo <rayvdKilo@gmail.com>
2023-01-15 16:05:53 +0300

    Put MatrixToArrayConverter.cs inside DataReaders.Utils namespace.

--- DataReaders/Utils/MatrixToArrayConverter.cs
+++ DataReaders/Utils/MatrixToArrayConverter.cs
@@ -1,18 +1,19 @@
-namespace DataReaders.Utils;
-
-public class MatrixToArrayConverter<T>
+namespace DataReaders.Utils
 {
-    public static T[] GetColumn(T[,] matrix, int columnNumber)
+    public class MatrixToArrayConverter<T>
     {
-        return Enumerable.Range(0, matrix.GetLength(0))
-            .Select(x => matrix[x, columnNumber])
-            .ToArray();
-    }
+        public static T[] GetColumn(T[,] matrix, int columnNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(0))
+                .Select(x => matrix[x, columnNumber])
+                .ToArray();
+        }
 
-    public static T[] GetRow(T[,] matrix, int rowNumber)
-    {
-        return Enumerable.Range(0, matrix.GetLength(1))
-            .Select(x => matrix[rowNumber, x])
-            .ToArray();
+        public static T[] GetRow(T[,] matrix, int rowNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(1))
+                .Select(x => matrix[rowNumber, x])
+                .ToArray();
+        }
     }
 }
